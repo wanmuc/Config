b1bd9499e6e9b987404d34e751343d2e33c0ad03
wanmuc <769535441@qq.com>
2023-05-01 21:16:52 +0800

    调整MysvrCfg.hpp代码

--- tool/MysvrCfg.hpp
+++ tool/MysvrCfg.hpp
@@ -9,16 +9,16 @@ class MysvrCfg {
     if (not ini.Load(file_name)) {
       return false;
     }
-    ini.GetStrValue("server", "ip", ip_, "");
-    ini.GetIntValue("server", "port", port_, 0);
     ini.GetIntValue("pool", "conn_pool_size", conn_pool_size_, 0);
+    ini.GetIntValue("server", "port", port_, 0);
+    ini.GetStrValue("server", "ip", ip_, "");
   }
+  int64_t conn_pool_size() { return conn_pool_size_; }
   int64_t port() { return port_; }
   std::string ip() { return ip_; }
-  int64_t conn_pool_size() { return conn_pool_size_; }
 
  public:
+  int64_t conn_pool_size_;
   int64_t port_;
   std::string ip_;
-  int64_t conn_pool_size_;
 };
